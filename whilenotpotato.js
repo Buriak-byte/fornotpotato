d321021eed5606d16f9cc288d840981fef5c9cb6
Buriak-byte <oleksii.buriak12@gmail.com>
2026-02-04 14:58:27 +0200

    eeee

---
+++ whilenotpotato.js
@@ -0,0 +1,11 @@
+//Вивести в консоль всі числа від 1 до 10 за допомогою циклу while.
+let timer = 0
+while(timer = 0; timer < 10; timer + 1;) {
+console.log(timer)
+}
+let i = 1;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
